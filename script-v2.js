38b0b007b30338441c6c5668b34136d282991454
techverse23 <techverse056@gmail.com>
2026-07-17 09:34:04 +0500

    Add scroll-to-top button functionality

--- script-v2.js
+++ script-v2.js
@@ -16,3 +16,19 @@ if (searchInput) {
     });
   });
 }
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 300) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+topBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
